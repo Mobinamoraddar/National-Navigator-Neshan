60c864ec2e0ee105bdd63e8d1f92b5849442344a
sepideh khodkavandi <sepideh@m.lo.agency>
2021-08-15 14:51:08 +0430

    fix : Remove extra useless codes

--- script.js
+++ script.js
@@ -9,13 +9,11 @@ iconclose.addEventListener("click",toggleMenu);
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
         menu.classList.remove("showMenu");
-        //  iconclose.style.display = "none";
         iconmenu.style.display = "block";
     }
     
     else {
       menu.classList.add("showMenu");
-    iconclose.style.display = "block";
       iconmenu.style.display = "none";
     }
      }
